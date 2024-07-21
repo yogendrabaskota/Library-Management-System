cc23b9e45a2171ef5a25c06b8e5da5a6438ff27f
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-21 16:24:43 +0545

    data stored successfully in database

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const express = require("express")
+const { connectDatabase } = require("./database/database")
+const Book = require("./model/bookModel")
 const app = express()
 
+
+connectDatabase()
+
+app.use(express.json())  
 app.get("/",(req,res)=>{
     res.status(200).json({
         message: "success"
     })
 })
 
-app.get("/books",async(req,res)=>{
+app.post("/book",async(req,res)=>{
+    //console.log(req.body)
+    const bookName = req.body.bookName
+    const bookPrice = req.body.bookPrice
+    const isbnNumber = req.body.isbnNumber
+    const authorName = req.body.authorName
+    const publishedAt = req.body.publishedAt
 
-    const books = await Book.find()
-    if(books.length == 0){
-        res.status(404).json({
-            message : "Empty books"
-        })
-    }
-    else {
-        res.status(200).json({
-            message : "books shown successfully"
-        })
-    }
+    await Book.create ({
+        bookName,
+        bookPrice,
+        isbnNumber,
+        authorName,
+        publishedAt
+    })
+    res.status(201).json({
+        // status : 201,
+         message : "Book is Successfully added to database"
+     })
+ 
 })
 
 PORT = 2100
---
+++ model/bookModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const bookSchema = new mongoose.Schema ({
+    bookName : {
+        type: String,
+        unique : true
+    },
+    bookPrice : {
+        type : String
+    },
+    isbnNumber : {
+        type : Number
+    },
+    authorName : {
+        type : String
+    },
+    publishedAt  : {
+        type : String
+    }
+})
+
+const Book = mongoose.model('Book',bookSchema)
+module.exports = Book
